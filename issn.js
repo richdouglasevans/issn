70cafc96f5d57b2b7f635901d6ed7415f487ad4e
richdouglasevans <rich.douglas.evans@gmail.com>
2015-08-06 22:05:09 +0100

    Browserify

--- issn.js
+++ issn.js
@@ -1,52 +1,61 @@
-!function() {
-  'use strict';
+!function(root) {
 
-  var issnPattern = '^(\\d{4})-?(\\d{3})([\\dX])$';
-  var isIssnStrict = new RegExp(issnPattern);
-  var isIssnLax = new RegExp(issnPattern, 'i');
+  var issn = function() {
+    'use strict';
 
-  function verifyIssn(issn) {
-    var matches = text(issn).match(isIssnStrict);
-    if (!matches) {
-      return false;
-    }
+    var issnPattern = '^(\\d{4})-?(\\d{3})([\\dX])$';
+    var isIssnStrict = new RegExp(issnPattern);
+    var isIssnLax = new RegExp(issnPattern, 'i');
 
-    var result = (matches[1] + matches[2])
+    function verifyIssn(issn) {
+      var matches = text(issn).match(isIssnStrict);
+      if (!matches) {
+        return false;
+      }
+
+      var result = (matches[1] + matches[2])
         .split('')
         .reverse()
-        .reduce(issnSum, 0)
-        % 11;
+        .reduce(issnSum, 0) % 11;
 
-    var actualCheckDigit = matches[3];
-    var expectedCheckDigit = calculateCheckDigit(result);
+      var actualCheckDigit = matches[3];
+      var expectedCheckDigit = calculateCheckDigit(result);
 
-    return expectedCheckDigit === actualCheckDigit;
-  }
-
-  function issnSum(sum, value, index) {
-    return sum + (value * (index + 2));
-  }
-
-  function calculateCheckDigit(number) {
-    var checkDigit = (number === 0) ? 0 : (11 - number);
-    if (checkDigit === 10) {
-      checkDigit = 'X';
+      return expectedCheckDigit === actualCheckDigit;
     }
-    return checkDigit.toString();
-  }
 
-  function text(o) {
-    return typeof o === 'string' ? o : '';
-  }
+    function issnSum(sum, value, index) {
+      return sum + (value * (index + 2));
+    }
 
-  verifyIssn.format = function formatIssn(issn) {
-    var matches = text(issn).match(isIssnLax);
-    return matches
+    function calculateCheckDigit(number) {
+      var checkDigit = (number === 0) ? 0 : (11 - number);
+      if (checkDigit === 10) {
+        checkDigit = 'X';
+      }
+      return checkDigit.toString();
+    }
+
+    function text(o) {
+      return typeof o === 'string' ? o : '';
+    }
+
+    verifyIssn.format = function formatIssn(issn) {
+      var matches = text(issn).match(isIssnLax);
+      return matches
         ? matches[1] + '-' + matches[2] + matches[3].toUpperCase()
         : undefined;
-  };
+    };
 
-  if (module && typeof module.exports !== 'undefined') {
-    module.exports = verifyIssn;
+    return verifyIssn;
+  }();
+
+  if (typeof exports !== 'undefined') {
+    if (module && typeof module.exports !== 'undefined') {
+      module.exports = issn;
+    }
+  } else {
+    root.issn = issn;
   }
-}();
+}(this);
+
